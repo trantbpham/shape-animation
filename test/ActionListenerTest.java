66bc6442266d5e832f0eb9bf05ce8114aee8cb1a
Tran <pham.tr@husky.neu.edu>
2020-04-11 18:48:30 -0400

    edited test

--- test/ActionListenerTest.java
+++ test/ActionListenerTest.java
@@ -1,10 +1,10 @@
-import
 
-import org.junit.Test;
-
-import java.awt.event.ActionListener;
-
-import static com.sun.tools.doclint.Entity.times;
+//
+//import org.junit.Test;
+//
+//import java.awt.event.ActionListener;
+//
+//import static com.sun.tools.doclint.Entity.times;
 //import static org.mockito.Mockito.*;
 //
 //public class ActionListenerTest {
